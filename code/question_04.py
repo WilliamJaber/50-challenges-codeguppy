120ef15bd613fd7badcdeb0db993ea280d37b14f
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-01-10 21:10:28 -0500

    Update question_04.py

--- code/question_04.py
+++ code/question_04.py
@@ -1,6 +1,6 @@
 """Print all the multiplication tables with numbers from 1 to 10"""
 
-def solution():
+def solution() -> None:
     for n in range(11):
         multiplicand = n
         for i in range(11):
