59a1fff9b478c16c869ba82e7a909a3ff43fdca9
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-01-10 21:04:24 -0500

    Update question_01.py
    The annotation '->' tells what the function returns. In Solution1, the function returns a list. In Solution2. the function returns None! Because the print() function returns None. This does not through errors.

--- code/question_01.py
+++ code/question_01.py
@@ -1,13 +1,13 @@
 """Print numbers from 1 to 10"""
 
-def solution():
+def solution1() -> lst:
     return [n for n in range(1, 11)]
 
 print(solution())
 
 
 
-def solution2():
+def solution2() -> None:
     n = 0
     while n < 10:
         n += 1
