4445e1ebcbabfa94786d91d3f6cea604a2d4d25c
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-03-23 20:40:58 -0400

    Update question_07.py
    This function return `int`

--- code/question_07.py
+++ code/question_07.py
@@ -1,6 +1,6 @@
 """Calculate the sum of even numbers greater than 10 and less than 30"""
 
-def solution() -> list:
+def solution() -> int:
     return sum([n for n in range(10, 30) if n%2 == 0])
 
 print(solution())
